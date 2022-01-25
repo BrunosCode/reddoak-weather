0fdd3e1b109127bb73681fcf1c6a4be909bd0893
brunEdo <edobruno.m@gmail.com>
2022-01-25 10:40:09 +0100

    weather app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>App Meteo</h1>
+    <CityInput @city="searchWeather" />
+    <WeatherComponent v-if="displayWeather"
+    :city="city" :weather="weather"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+import CityInput from './components/CityInput.vue'
+import WeatherComponent from './components/WeatherComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CityInput,
+    WeatherComponent
+  },
+  data() {
+    return {
+      city: "",
+      weather: {},
+      displayWeather: false
+    }
+  },
+  methods: {
+    getWeather() {
+      axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          q: this.city,
+          lang: "it",
+          appid: "567a30ac0bf63a04687e6450b6c7f1a9"
+        }
+      })
+      .then((res) => {
+        this.weather = res.data.weather[0];
+        console.log(res.data.weather[0], this.city);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    },
+    searchWeather(city) {
+      this.city = city[0].toUpperCase() + city.slice(1);
+      this.displayWeather = true;
+      this.getWeather();
+    }
   }
 }
 </script>
 
 <style lang="scss">
+// Reset
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/CityInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="CityInput">
+    <input v-model="city" type="text" name="city" id="city">
+    <button @click="emitWeather" @keypress.enter="emitWeather">Get Weather</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityInput',
+  data() {
+    return {
+      city: ""
+    }
+  },
+  methods: {
+    emitWeather() {
+      this.$emit("city", this.city);
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
